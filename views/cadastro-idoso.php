d93b9174b1cd4dfd257ba36c29038ec9a14af30e
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2020-06-20 21:34:04 -0300

    Update cadastro-idoso.php

--- views/cadastro-idoso.php
+++ views/cadastro-idoso.php
@@ -14,11 +14,11 @@
         <label class="float">Nome do Idoso:</label>
         <input class="float w100 h40" type="text" id="nome" name="nome" placeholder="Nome Completo" required>
         
-        <!--<select class="float w100 h40" name="sexo" id="sexo">
+        <select class="float w100 h40" name="sexo" id="sexo">
             <option value="">Sexo</option>
             <option value="masculino">Masculino</option>
             <option value="feminino">Feminino</option>
-        </select-->
+        </select>
 
         <label class="float">Cidade:</label>
         <input class="float w100 h40" type="text" id="cidade" name="cidade" required>
@@ -26,22 +26,23 @@
         <label class="float">Bairro:</label>    
         <input class="float w100 h40" type="text" id="bairro" name="bairro" required>
 
-        <!--select class="float w100 h40" name="categoria" id="categoria" required>
+        <select class="float w100 h40" name="categoria" id="categoria" required>
             <option value="">Quero Ajuda</option>
             <option value="educacaoFinanceira">Educação Financeira</option>
             <option value="educacaoTecnologica">Educação Tecnológica</option>
             <option value="combateIsolamento">Combate ao Isolamento</option>
-        </select-->
+        </select>
 
-        <!--select class="float w100 h40" name="online" id="online" required>
+        <select class="float w100 h40" name="online" id="online" required>
             <option value="">Pode ser Feito Online</option>
             <option value="sim">Sim - Tenho condição de ser atendido à distância</option>
             <option value="nao">Não</option>
         </select>
-        <input class="float w100 h40" type="fone" id="contato" name="contato" placeholder="Contato:" required-->
+        <label class="float">Contato da Pessoa Responsável Legal ,parente,ou pessoa próxima:</label>
+        <input class="float w100 h40" type="fone" id="contato" name="contato" placeholder="Contato:" required>
 
-      
-        <input class="float w100 h40" type="fone" id="anoNascimento" name="anoNascimento" placeholder="anoNascimento" required>
+        <label class="float">Ano de Nascimento(somente números)</label>
+        <input class="float w100 h40" type="fone" id="anoNascimento" name="anoNascimento" placeholder="4 Dígitos " required>
 
               
 
