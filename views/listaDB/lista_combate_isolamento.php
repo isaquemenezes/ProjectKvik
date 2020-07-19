64a5efb30a042d0b16a783d753a352fa213283ca
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2020-07-18 21:02:02 -0300

    Update lista_combate_isolamento.php

--- views/listaDB/lista_combate_isolamento.php
+++ views/listaDB/lista_combate_isolamento.php
@@ -12,79 +12,80 @@
 	$BFetch=$crud->selectDB(
 					"*", 
 					"users_idoso", 
-					" WHERE categoria='Educacao Tecnologica' and ead='nao' ORDER BY idIdoso  LIMIT $inicio, $qnt_result_pg", 
+					"WHERE categoria='Combate ao Isolamento' and ead='sim' ORDER BY idIdoso  LIMIT $inicio, $qnt_result_pg", 
 					array(
 
 					)
 				);
     while($Fetch=$BFetch->fetch(PDO::FETCH_ASSOC)){
-	?>
+?>
+
 	<ul>
 		<li>
 			<div class="card-body">
 				<img  src="./assets/images/idososentado.jpg">
 			    <p><?php echo $Fetch['nome']; ?></p>
+				<p><?php echo $Fetch['email']; ?></p>
 			    <p><?php echo $Fetch['anoNascimento']; ?></p>
+				<p><?php echo $Fetch['sexo']; ?></p>
+				<p><?php echo $Fetch['contato']; ?></p>
 			    <p><?php echo $Fetch['cidade']; ?></p>	
 			    <p><?php echo $Fetch['bairro']?></p>
+				<p><?php echo $Fetch['ead']; ?></p>
 			</div>
 		</li>			
-        <?php 
-            }
-        ?>
+<?php 
+    }
+?>
+
 	</ul>	 
 
 <?php 
-$crud=new ClassCrud();
-//Paginação - Soma a quantidade de usuários	
-$result_page=$crud->selectDB(
-					"COUNT(*) AS num_result", 
-					"users_idoso",
-					"WHERE categoria='Educacao Tecnologica' and ead='nao'", 
-					array(
-					)
-				);
-$row_pg = $result_page->fetch(PDO::FETCH_ASSOC);
+	$crud=new ClassCrud();
+	//Paginação - Soma a quantidade de usuários	
+	$result_page=$crud->selectDB(
+						"COUNT(*) AS num_result", 
+						"users_idoso",
+						"WHERE categoria='Combate ao Isolamento' and ead='sim'", 
+						array(
+						)
+					);
+	$row_pg = $result_page->fetch(PDO::FETCH_ASSOC);
 
-//Quantidade de pagina para enviar para última página
-$qnt_page=ceil($row_pg['num_result'] / $qnt_result_pg);
+	//Quantidade de pagina para enviar para última página
+	$qnt_page=ceil($row_pg['num_result'] / $qnt_result_pg);
 
+	//Limitar os links antes e depois da página atual, ou seja , se estou na page 3 mostra 1 2 e 4 5
+	$max_links = 2;
 
-//Limitar os links antes e depois da página atual, ou seja , se estou na page 3 mostra 1 2 e 4 5
-$max_links = 2;
-
-
-echo "<nav aria-label='paginacao'>
-  <ul class='pagination'>
-    <li class='page-item'>
-   <span class='page-link'><a href='#' onclick='listar_user(1, $qnt_result_pg)'>Primeira</a></span>
-    </li>";
-    for ($page_antes=$pagina-$max_links; $page_antes <= $pagina-1; $page_antes++) { 
-	//Retirar o 0 à esquerda, quando o user estar n primeira page, ou seja , o número zero, não é exibido
-		if($page_antes >= 1){
-			echo " <li class='page-item'>
-			<a class='page-link' href='#' onclick='listar_user($page_antes, $qnt_result_pg)'>$page_antes</a></li>"; 
+	// iNÍCIO <nav>
+	echo "<nav aria-label='paginacao'><ul class='pagination'><li class='page-item'>
+		<span class='page-link'><a href='#' onclick='listar_user(1, $qnt_result_pg)'>Primeira</a></span>
+		</li>";
+		for ($page_antes=$pagina-$max_links; $page_antes <= $pagina-1; $page_antes++) { 
+		//Retirar o 0 à esquerda, quando o user estar n primeira page, ou seja , o número zero, não é exibido
+			if($page_antes >= 1){
+				echo " <li class='page-item'>
+				<a class='page-link' href='#' onclick='listar_user($page_antes, $qnt_result_pg)'>$page_antes</a></li>"; 
+			}
 		}
-	}
 
-    //Página q o user be
-   echo  "<li class='page-item active' >
-      <span class='page-link'>$pagina</span></li>";
+	//Página atual
+	echo  "<li class='page-item active' ><span class='page-link'>$pagina</span></li>";
 
-    //Paginas Posteriores
-	for ($page_dep=$pagina + 1; $page_dep <= $pagina + $max_links; $page_dep++) { 
-		//Eliminando página q não contém dados
-		if($page_dep <= $qnt_page){
-			 echo " <li class='page-item'>
-			 <a class='page-link'  href='#' onclick='listar_user($page_dep, $qnt_result_pg)'>$page_dep</a></li>"; 
-	 	}
-	 } 
-    
-    echo "<li class='page-item'>
-	        <span class='page-link'><a href='#' onclick='listar_user($qnt_page, $qnt_result_pg)'>ùltima</a></span>
-            </li> </ul>
-            </nav>"; // ClOSE </nav>
-            
+		//Paginas Posteriores
+		for ($page_dep=$pagina + 1; $page_dep <= $pagina + $max_links; $page_dep++) { 
+			//Eliminando página q não contém dados
+			if($page_dep <= $qnt_page){
+				echo " <li class='page-item'>
+				<a class='page-link'  href='#' onclick='listar_user($page_dep, $qnt_result_pg)'>$page_dep</a></li>"; 
+			}
+		} 
+		
+		echo "<li class='page-item'>
+				<span class='page-link'><a href='#' onclick='listar_user($qnt_page, $qnt_result_pg)'>última</a></span>
+				</li> </ul></nav>"; // ClOSE </nav>
+				
 
 
 
