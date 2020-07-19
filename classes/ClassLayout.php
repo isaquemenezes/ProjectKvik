27631cc33ffdc6cf389840d156aed79a617ee60e
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2020-07-18 21:05:48 -0300

    Update ClassLayout.php

--- classes/ClassLayout.php
+++ classes/ClassLayout.php
@@ -120,53 +120,6 @@
             body.part_policy main th {border-color: #FFF;background-color: #CCC;border-width: 1px;margin: 15px 0 15px 0;padding: 10px;}
             </style>\n";
         
-
-            $html.="</head>\n\n";
-            $html.="<body> \n";
-            echo $html;
-        }
-
-        public static function setHeadOld($title, $description , $author='kvik'){
-            $html="<!doctype html>\n";
-            $html.="<html lang='pt-br'>\n";
-            $html.="<head>\n";
-            $html.="  <meta charset='UTF-8'>\n";
-            $html.="  <meta name='viewport' content='width=device-width, initial-scale=1'>\n";
-            $html.="  <meta name='author' content='$author'>\n";
-            $html.="  <meta name='format-detection' content='telephone=no'>\n";
-            $html.="  <meta name='description' content='$description'>\n";
-            $html.="  <title>$title</title>\n";
-            
-            $html.="  <link rel='icon' type='image/png' href='".DIRPAGE."./assets/logo.ico'>\n"; 
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/animate.css'>\n";
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/common.css'>\n";
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/corporate.css'>\n";
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/variables-corporate.css'>\n";
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./asset/css/fileinput.min.css'>\n";
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/main.css'>\n";
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/main2.css'>\n";
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/reset.css'>\n";
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/configSvg.css'>\n";
-                
-            $html.="</head>\n\n";
-            $html.="<body> \n";
-            echo $html;
-        }
-
-        #setHead2
-        public static function setHead2($title, $description , $author='kvik'){
-            $html="<!doctype html>\n";
-            $html.="<html lang='pt-br'>\n";
-            $html.="<head>\n";
-            $html.="  <meta charset='UTF-8'>\n";
-            $html.="  <meta name='viewport' content='width=device-width, initial-scale=1'>\n";
-            $html.="  <meta name='author' content='$author'>\n";
-            $html.="  <meta name='format-detection' content='telephone=no'>\n";
-            $html.="  <meta name='description' content='$description'>\n";
-            $html.="  <title>$title</title>\n";
-           
-            $html.="  <link rel='stylesheet' href='".DIRPAGE."./assets/css/main3.css'>\n";
-
             $html.="</head>\n\n";
             $html.="<body> \n";
             echo $html;
