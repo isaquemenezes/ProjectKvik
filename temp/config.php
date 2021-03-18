ba2796df8423fcdf7ef0a14f834d74e525e9d593
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2021-03-18 08:31:26 -0300

    Update config.php

--- temp/config.php
+++ temp/config.php
@@ -14,23 +14,23 @@ define('DIRJS', DIRPAGE.'assets/js/');
 
 #Acesso ao db
 define('HOST',"localhost");
-define('DB',"conex009_sistema");
-define('USER',"conex009_isaque");
-define('PASS',"NARUTO*0001");
+define('DB',"");
+define('USER',"");
+define('PASS',"");
 
 #Informações do servidor de email
-define("HOSTMAIL","mail.conexaoacara.com");
-define("USERMAIL","contato@conexaoacara.com");
+define("HOSTMAIL","");
+define("USERMAIL","");
 //define("PASSMAIL","");
-define("PASSMAIL","NARUTO*0001email");
+define("PASSMAIL","");
 define ("MAIL",[
     "from_name" => "Equipe Conexão Acará",
     "from_email" => "contato@conexaoacara.com"
 ]);
 
 #Outras informações GOOGLE CAPTACHA
-define("SITEKEY", "6LdU-t8UAAAAAPk2EVlzqCvpa3QZ05cOa7TOmH_X");
-define("SECRETKEY","6LdU-t8UAAAAAMRDDgUHX2t-BXY9Q7NQpqWoXMPY");
+define("SITEKEY", "");
+define("SECRETKEY","");
 
 #Outros
 define("DOMAIN",$_SERVER["HTTP_HOST"]);
